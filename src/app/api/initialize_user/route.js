ca3572ec80896dad95b23e77b13a1df7eb16a598
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2024-07-21 18:09:10 +0300

    sca kullanıcı oluşturuldu. üstünde işlem yapıldı.

--- src/app/api/initialize_user/route.js
+++ src/app/api/initialize_user/route.js
@@ -13,7 +13,11 @@ export const initialize_user = async () => {
       Authorization: `Bearer  ${process.env.NEXT_PUBLIC_API_KEY}`,
       "X-User-Token": `${process.env.NEXT_PUBLIC_USER_TOKEN}`,
     },
-    data: { idempotencyKey: idempotencyKey, blockchains: ["MATIC-AMOY"] },
+    data: {
+      idempotencyKey: idempotencyKey,
+      accountType: "SCA",
+      blockchains: ["MATIC-AMOY"],
+    },
   };
 
   return axios
